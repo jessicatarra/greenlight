77158acaa13099aae71a546951fd6763c80cb6be
Jessica Tarra <jessica@tarralva.com>
2023-11-03 16:21:50 -0300

    7.3: Add fetch movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/jessicatarra/greenlight/internal/data"
 	"github.com/jessicatarra/greenlight/internal/validator"
 	"net/http"
-	"time"
 )
 
 func (app *application) createMovieHandler(writer http.ResponseWriter, request *http.Request) {
@@ -60,19 +60,18 @@ func (app *application) showMovieHandler(writer http.ResponseWriter, request *ht
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(writer, request)
+		default:
+			app.serverErrorResponse(writer, request, err)
+		}
+		return
 	}
 
-	err = app.writeJSON(writer, http.StatusOK,
-		envelope{
-			"movie": movie,
-		}, nil)
+	err = app.writeJSON(writer, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.serverErrorResponse(writer, request, err)
 	}
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"github.com/jessicatarra/greenlight/internal/validator"
 	"github.com/lib/pq"
 	"time"
@@ -50,7 +51,37 @@ func (m MovieModel) Insert(movie *Movie) error {
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	return nil, nil
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `
+        SELECT id, created_at, title, year, runtime, genres, version
+        FROM movies
+        WHERE id = $1`
+
+	var movie Movie
+
+	err := m.DB.QueryRow(query, id).Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		pq.Array(&movie.Genres),
+		&movie.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &movie, nil
 }
 
 func (m MovieModel) Update(movie *Movie) error {
