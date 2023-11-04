75c4f175da7c5ef3373fdbd8a15d03ae9295e809
Jessica Tarra <jessica@tarralva.com>
2023-11-04 16:40:21 -0300

    9.7: Add Paginating Lists

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -159,12 +159,16 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
         FROM movies
         WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
         AND (genres @> $2 OR $2 = '{}')     
-        ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
+        ORDER BY %s %s, id ASC
+        LIMIT $3 OFFSET $4
+        `, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
+	args := []interface{}{title, pq.Array(genres), filters.limit(), filters.offset()}
+
+	rows, err := m.DB.QueryContext(ctx, query, args...)
 	if err != nil {
 		return nil, err
 	}
