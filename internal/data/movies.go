4bd62b1750f23f992125377c8e414bc61728cd62
Jessica Tarra <jessica@tarralva.com>
2023-11-04 11:05:56 -0300

    9.4: add filter by title or genres

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -156,12 +156,14 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	query := `
         SELECT id, created_at, title, year, runtime, genres, version
         FROM movies
+        WHERE (LOWER(title) = LOWER($1) OR $1 = '') 
+        AND (genres @> $2 OR $2 = '{}')
         ORDER BY id`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
